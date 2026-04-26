d7513d28652878de1db2c8d8254a89e4ee75d240
Raphael J. Sandor <raphael.sandor@str.us>
2026-04-26 13:45:41 -0400

    more wasm updates

--- hello_world.main.cpp
+++ hello_world.main.cpp
@@ -1,5 +1,9 @@
 #include "hello_imgui/hello_imgui.h"
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
 int main(int , char *[])
 {
 #ifdef ASSETS_LOCATION
@@ -8,9 +12,17 @@ int main(int , char *[])
     auto guiFunction = []() {
         ImGui::Text("Hello, ");                    // Display a simple label
         HelloImGui::ImageFromAsset("world.jpg");   // Display a static image
-        if (ImGui::Button("Bye!"))                 // Display a button
-            // and immediately handle its action if it is clicked!
+        if (ImGui::Button("Bye!")) {               // Display a button
+            // For web/extension builds, close the window
+#ifdef __EMSCRIPTEN__
+            EM_ASM({
+                window.close();
+            });
+#else
+            // For desktop builds, exit the app
             HelloImGui::GetRunnerParams()->appShallExit = true;
+#endif
+        }
      };
     HelloImGui::Run(guiFunction, "Hello, globe", true);
     return 0;
